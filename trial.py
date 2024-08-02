ab1f28674eae89d53b8b4d346d17c48ee065a5eb
anaschen29 <chen.anas@gmail.com>
2024-08-01 23:11:08 -0400

    viva l'algerie

--- trial.py
+++ trial.py
@@ -1,19 +1,8 @@
-import numpy as np
+class VivalAlgerie():
+    def __init__(self):
+        pass
 
-array = [(2,5), (4,18), (12,9), (2,7), (4, 20)]
+def f(class_function):
+    return class_function()
 
-def new_average_consecutive(array):
-    table = {}
-    for n,v in array:
-      try:
-          table[n].append(v)
-      except:
-          table[n] = [v]
-
-    output = []
-    for n in sorted(table):
-       output.append(np.average(table[n]))
-    
-    return output
-
-print(new_average_consecutive(array))
+print(f(VivalAlgerie))
